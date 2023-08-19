63a0686d2a520d13a687b843afa9ddc60c83a784
TallTall22 <syuns1205@gmail.com>
2023-08-20 05:13:01 +0800

    add socket messages

--- sockets/index.js
+++ sockets/index.js
@@ -1,5 +1,11 @@
 module.exports = (io) => {
-  const messages=[]
+  const messages=[
+    {name:'seller001',message:'各位顧客您好，有任何問題都可以在這邊發問'},
+    {name:'buyer789',message:'好的'},
+    {name:'buyer678',message:'知道了'},
+    {name:'seller001',message:'感謝各位回覆，歡迎繼續選購本店的商品'},
+    {name:'buyer789',message:'不客氣'}
+  ]
   io.on('connection', (socket) => {
 
     io.emit('message',messages)
