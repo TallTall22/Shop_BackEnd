22a2a2462acd4c31f7eac70edd50dad3b8b4a990
TallTall22 <syuns1205@gmail.com>
2024-02-18 00:57:51 +0800

    add productList

--- seeders/20230715150213-add-seed-to-product.js
+++ seeders/20230715150213-add-seed-to-product.js
@@ -1,23 +1,15 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-const faker=require('faker')
+const productList =require('../productList.json')
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const categories = await queryInterface.sequelize.query(
-      'Select id from Categories;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT }
-    )
-    await queryInterface.bulkInsert('Products',Array.from({length:50},()=>({
-      name:faker.name.findName(),
-      price:Math.floor(Math.random()*200)*10+200,
-      description:faker.lorem.text(),
-      quantity:Math.floor(Math.random()*200)+100,
-      image:`https://loremflickr.com/400/300/clothes/?random=${Math.random() * 100}`,
-      category_id:categories[Math.floor(Math.random() * categories.length)].id,
-      is_selling:true,
+    const productsWithTimestamps = productList.map(product => ({
+      ...product,
       created_at: new Date(),
       updated_at: new Date()
-    })))
+    }));
+
+    await queryInterface.bulkInsert('Products', productsWithTimestamps);
   },
 
   async down (queryInterface, Sequelize) {
